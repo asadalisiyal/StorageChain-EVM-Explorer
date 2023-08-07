e6886051dce660c23be63843a86b7d72d9b51215
taimooraslam <taimoor.aslam@invozone.com>
2023-08-07 15:56:30 +0500

    text fixed

--- src/App.tsx
+++ src/App.tsx
@@ -414,7 +414,7 @@ function App(props: any) {
           >
             <Grid item md={6} xs={12}>
               <h1 style={{ color: "#fff", marginBottom: "0.5rem" }}>
-                The Storage Chain Blockchain Explorer
+                The StorageChain Blockchain Explorer
               </h1>
               <InputBase
                 className="input-field"
